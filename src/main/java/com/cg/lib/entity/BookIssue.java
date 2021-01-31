40a0471f9f43c2c37541c5fef7ec78aac02c0a02
yashveer007 <61536118+yashveer007@users.noreply.github.com>
2021-01-31 09:18:30 +0530

    Update BookIssue.java

--- src/main/java/com/cg/lib/entity/BookIssue.java
+++ src/main/java/com/cg/lib/entity/BookIssue.java
@@ -20,7 +20,15 @@ public class BookIssue {
 	private LocalDate returnBookDate;
 	
 	@OneToMany
-	private Books book;
+	private List<Books> book;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 
 	public LocalDate getIssueBookDate() {
 		return issueBookDate;
@@ -38,41 +46,32 @@ public class BookIssue {
 		this.returnBookDate = returnBookDate;
 	}
 
-	public Books getBook() {
+	public List<Books> getBook() {
 		return book;
 	}
 
-	public void setBook(Books book) {
+	public void setBook(List<Books> book) {
 		this.book = book;
 	}
 
-	
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	
-	
 	@Override
 	public String toString() {
 		return "BookIssue [userName=" + userName + ", issueBookDate=" + issueBookDate + ", returnBookDate="
 				+ returnBookDate + ", book=" + book + "]";
 	}
 
-	public BookIssue() {
-		super();
-	}
-
-	public BookIssue(String userName, LocalDate issueBookDate, LocalDate returnBookDate, Books book) {
+	public BookIssue(String userName, LocalDate issueBookDate, LocalDate returnBookDate, List<Books> book) {
 		super();
 		this.userName = userName;
 		this.issueBookDate = issueBookDate;
 		this.returnBookDate = returnBookDate;
 		this.book = book;
 	}
+
+	public BookIssue() {
+		super();
+	}
+
 	
 	
 }
